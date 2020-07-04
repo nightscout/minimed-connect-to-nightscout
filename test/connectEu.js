bc1e47a09e1eb72ebacfe9c3b59e61595f8aa82a
Frigyes Bartha <frigyes.bartha@gmail.com>
2020-07-04 17:13:33 +0200

    connection test for EU and US server

---
+++ test/connectEu.js
@@ -0,0 +1,29 @@
+/* jshint node: true */
+/* globals describe, it */
+"use strict";
+
+process.env['MMCONNECT_SERVER'] = "EU";
+
+var _ = require('lodash'),
+  expect = require('expect.js');
+
+var carelink = require('../carelink.js');
+
+describe('connectEu()', function() {
+  var client = carelink.Client({
+    username: "nstesteu",
+    password: "ournightscouteutest",
+  });
+
+  it('should save without error', function (done) {
+    client.fetch(function (err, data) {
+      if (err) {
+        done(err)
+      }
+      else {
+        expect(data).to.have.property('bgunits');
+        done();
+      }
+    })
+  });
+});
