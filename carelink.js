c4850ccb8a187619bfb9549fd1f7df8eaf422a05
Ben West <bewest@gmail.com>
2020-12-17 17:09:19 -0800

    matches @fredmk's axios branch much closer

--- carelink.js
+++ carelink.js
@@ -266,8 +266,8 @@ var Client = exports.Client = function (options) {
         try {
             let maxRetry = 1; // No retry
             for (let i = 1; i <= maxRetry; i++) {
-                await checkLogin();
                 try {
+                    await checkLogin();
                     data = (await getConnectData()).data;
                     break;
                 } catch (e1) {
@@ -275,11 +275,6 @@ var Client = exports.Client = function (options) {
 
                     if (i === maxRetry)
                         throw e1;
-
-                    if (e1.response && e1.response.status === 401) {
-                        // reauth
-                        cookieJar.removeAllCookiesSync();
-                    }
 
                     let timeout = retryDurationOnAttempt(i);
                     await sleep(1000 * timeout);
