3bf5193b7dd7f98cb74ab733f36521de1ad1a85a
Stijn Brouwers <stijn@bdcommit.com>
2022-10-28 20:14:46 +0200

    feat(run): Add missing medtronic entries and calibration time

--- run.js
+++ run.js
@@ -1,5 +1,6 @@
 /* jshint node: true */
 "use strict";
+const { match } = require('assert');
 const fs = require('fs');
 
 var carelink = require('./carelink'),
@@ -33,6 +34,7 @@ var config = {
   maxRetryDuration: parseInt(readEnv('CARELINK_MAX_RETRY_DURATION', carelink.defaultMaxRetryDuration), 10),
   verbose: !readEnv('CARELINK_QUIET', true),
   deviceInterval: 5.1 * 60 * 1000,
+  maxNightscoutDiff: 150
 };
 
 if (!config.username) {
@@ -59,6 +61,60 @@ var filterDeviceStatus = filter.makeRecencyFilter(function(item) {
   return new Date(item['created_at']).getTime();
 });
 
+function getMatchingNightscoutSgv(minimedSgv,nightscoutSgvs) {
+  var out = [];
+
+  nightscoutSgvs.forEach(nightscoutSgv => {
+    if(nightscoutSgv.sgv === minimedSgv.sgv) {
+      var timeDiff = nightscoutSgv.date - minimedSgv.date;
+      if(timeDiff >= 0 && timeDiff <= config.maxNightscoutDiff*1000) {
+        out.push(nightscoutSgv);
+      }
+    }
+  });
+
+  return out;
+}
+
+function filterMissingSgvs(minimedSgvs,nightscoutSgvs) {
+  var out = [];
+
+  var matchCount = 0;
+  var totalTimeDiff = 0;
+  minimedSgvs.forEach(minimedSgv => {
+
+    var matchingNightscoutSgvs = getMatchingNightscoutSgv(minimedSgv,nightscoutSgvs);
+    if(matchingNightscoutSgvs.length === 0) {
+      //console.warn(`> Adding ${minimedSgv.sgv} @ ${new Date(minimedSgv.date).toLocaleString()}`);
+      out.push(minimedSgv);
+    } else if (matchingNightscoutSgvs.length > 1) {
+      console.error(`Something went wrong: More than 1 matching nightscout entry was returned for ${minimedSgv.sgv} @ ${new Date(minimedSgv.date).toLocaleString()}`);
+      matchingNightscoutSgvs.forEach(matchingNightscoutSgv => {
+        console.error(`\tNS match = ${matchingNightscoutSgv.sgv} @ ${new Date(matchingNightscoutSgv.date)}`)
+      });
+    } else {
+      var matchingNightscoutSgv = matchingNightscoutSgvs[0];
+      matchCount++;
+      totalTimeDiff += matchingNightscoutSgv.date - minimedSgv.date;
+    }
+  });
+
+  if(matchCount < 5) {
+    console.log(`Not enough nightscout entries found, not uploading anything`);
+    return [];
+  }
+
+  let averageTimeDiff = Math.round(totalTimeDiff/matchCount);
+  console.log(`average time diff: ${averageTimeDiff}`);
+  out.forEach(svg => {
+    let dateBefore = svg.date;
+    svg.date += averageTimeDiff;
+    console.warn(`> Adding ${svg.sgv} @ ${new Date(dateBefore).toLocaleString()} =>${new Date(svg.date).toLocaleString()}`);
+  });
+
+  return out;
+}
+
 function uploadMaybe(items, endpoint, callback) {
   if (items.length === 0) {
     logger.log('No new items for ' + endpoint);
@@ -77,10 +133,7 @@ function uploadMaybe(items, endpoint, callback) {
 function deleteFileIfExists(path) {
   fs.exists(path, function(exists) {
     if(exists) {
-        console.log('File exists. Deleting now ...');
         fs.unlinkSync(path);
-    } else {
-        console.log('File not found, so not deleting.');
     }
   });
 }
@@ -93,37 +146,46 @@ function requestLoop() {
         setTimeout(requestLoop, config.deviceInterval);
       } else {
         var dataPath = '/Users/asopleo/workspace/minimed-connect-to-nightscout/carelink-data.json';
-        var transformedPath = '/Users/asopleo/workspace/minimed-connect-to-nightscout/carelink-transformed.json';
 
         var jsonData = JSON.stringify(data,undefined,4);
         deleteFileIfExists(dataPath);
         fs.writeFileSync(dataPath,jsonData);
         let transformed = transform(data, config.sgvLimit);
-        var transformedData = JSON.stringify(transformed.devicestatus,undefined,4);
-        deleteFileIfExists(transformedPath);
-        fs.writeFileSync(transformedPath,transformedData);
-
+        
         // Because of Nightscout's upsert semantics and the fact that CareLink provides trend
         // data only for the most recent sgv, we need to filter out sgvs we've already sent.
         // Otherwise we'll overwrite existing sgv entries and remove their trend data.
         let newSgvs = filterSgvs(transformed.entries);
 
-        // Nightscout's entries collection upserts based on date, but the devicestatus collection
-        // does not do the same for created_at, so we need to de-dupe them here.
-        let newDeviceStatuses = filterDeviceStatus(transformed.devicestatus);
+        nightscout.get(entriesUrl+'?find[device]=Leonneke%20%26lt%3B3&count='+(config.sgvLimit+5),function(err, response) {
+          const nightscoutSgvs = response.body;
 
-        // Calculate interval by the device next upload time
-        let interval = config.deviceInterval - (data.currentServerTime - data.lastMedicalDeviceDataUpdateServerTime);
-        if (interval > config.deviceInterval || interval < 0)
-          interval = config.deviceInterval;
+          let missingSgvs = filterMissingSgvs(newSgvs,nightscoutSgvs);
 
-        logger.log(`Next check ${Math.round(interval / 1000)}s later (at ${new Date(Date.now() + interval)})`)
+          // Nightscout's entries collection upserts based on date, but the devicestatus collection
+          // does not do the same for created_at, so we need to de-dupe them here.
+          let newDeviceStatuses = filterDeviceStatus(transformed.devicestatus);
+          
+          newDeviceStatuses[0].pump = {
+            reservoir: data.reservoirRemainingUnits,
+            status: {
+              status: ' - Calibrate@' +new Date(new Date(newDeviceStatuses[0].created_at).valueOf()+data.timeToNextCalibrationMinutes*60*1000).toLocaleString()
+            }
+          }
 
-        //uploadMaybe(newSgvs, entriesUrl, function() {
-          uploadMaybe(newDeviceStatuses, devicestatusUrl, function() {
-            setTimeout(requestLoop, interval);
+          // Calculate interval by the device next upload time
+          let interval = config.deviceInterval - (data.currentServerTime - data.lastMedicalDeviceDataUpdateServerTime);
+          if (interval > config.deviceInterval || interval < 0)
+            interval = config.deviceInterval;
+
+          logger.log(`Next check ${Math.round(interval / 1000)}s later (at ${new Date(Date.now() + interval)})`)
+
+          uploadMaybe(missingSgvs, entriesUrl, function() {
+            uploadMaybe(newDeviceStatuses, devicestatusUrl, function() {
+              setTimeout(requestLoop, interval);
+            });
           });
-        //});
+        });
       }
     });
   } catch (error) {
